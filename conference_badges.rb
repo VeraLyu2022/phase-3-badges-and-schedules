7f3acd71eb70f6b3a52d868b1ad40959a6636a04
Vera Lyu <465784942@qq.com>
2023-04-02 11:28:58 +1000

    got a small help from ChatGPT but did most of the lab myself. so proud

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    
+    array.map do |name|
+        "Hello, my name is #{name}."
+    end
+end
+
+def assign_rooms(array)
+    messages = []
+    array.each_with_index do |name, index|
+        messages << "Hello, #{name}! You'll be assigned to room #{index+1}!"
+    end
+    messages
+end
+
+
+def printer(array)
+    batch_badge_creator(array).each {|welcome_messages| puts welcome_messages }
+   assign_rooms(array).each{|assign_messages| puts assign_messages}
+end
